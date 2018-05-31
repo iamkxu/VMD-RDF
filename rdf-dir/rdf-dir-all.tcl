efdea38a37f22044f2a68e226c719085719c37dc
Gaia <34270312+xukuiyes@users.noreply.github.com>
2018-05-31 20:01:06 +0200

    update

--- rdf-dir/rdf-dir-all.tcl
+++ rdf-dir/rdf-dir-all.tcl
@@ -1,5 +1,5 @@
-source rdf-dir-EMIM-BF4.tcl
-#source rdf-dir-EMIM-Cl.tcl
-source rdf-dir-EMIM-FSI.tcl
-source rdf-dir-EMIM-OTF.tcl
-source rdf-dir-EMIM-TFSI.tcl
+source C:/Users/RTS-Kui/Documents/GitHub/VMD-RDF/rdf-dir/rdf-dir-EMIM-BF4.tcl
+#source C:/Users/RTS-Kui/Documents/GitHub/VMD-RDF/rdf-dir/rdf-dir-EMIM-Cl.tcl
+source C:/Users/RTS-Kui/Documents/GitHub/VMD-RDF/rdf-dir/rdf-dir-EMIM-FSI.tcl
+source C:/Users/RTS-Kui/Documents/GitHub/VMD-RDF/rdf-dir/rdf-dir-EMIM-OTF.tcl
+source C:/Users/RTS-Kui/Documents/GitHub/VMD-RDF/rdf-dir/rdf-dir-EMIM-TFSI.tcl
